2b910a2f2f981f54b4e7bc6637bc058837d4118a
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-03-31 21:18:37 -0500

    ✨ Fixed-Time Motion with Input Shaping by Ulendo (#25394)
    Co-authored-by: Ulendo Alex <alex@ulendo.io>

--- Marlin/src/gcode/gcode.h
+++ Marlin/src/gcode/gcode.h
@@ -1039,6 +1039,10 @@ private:
     static void M486();
   #endif
 
+  #if ENABLED(FT_MOTION)
+    static void M493();
+  #endif
+
   static void M500();
   static void M501();
   static void M502();
